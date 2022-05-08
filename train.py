e12a1df6c5fd3b2a5a209016fa13e95d3dc21a5a
kyle65463 <kyle65463@gmail.com>
2022-05-08 16:43:35 +0800

    Optimizer

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
-import json
+import argparse
+import math
 from torch.utils.data import DataLoader
 from accelerate import Accelerator
 from datasets import load_dataset
-from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorForSeq2Seq
-import argparse
+from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorForSeq2Seq, AdamW
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Finetune a transformers model on a summarization task")
@@ -26,6 +26,21 @@ def parse_args():
         "--batch_size",
         type=int,
         default=2,
+    )
+    parser.add_argument(
+        "--weight_decay", 
+        type=float, 
+        default=0.0, 
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=5e-5,
+    )
+    parser.add_argument(
+        "--num_epochs", 
+        type=int, 
+        default=3,
     )
     args = parser.parse_args()
     return args
@@ -69,12 +84,29 @@ processed_datasets = raw_datasets.map(
     desc="Running tokenizer on dataset",
 )
 train_dataset = processed_datasets["train"]
-print(train_dataset[1])
-print(tokenizer.pad_token_id)
 data_collator = DataCollatorForSeq2Seq(
     tokenizer, model=model, label_pad_token_id=tokenizer.pad_token_id
 )
 
 train_dataloader = DataLoader(
     train_dataset, shuffle=True, collate_fn=data_collator, batch_size=args.batch_size
+)
+
+# Optimizer
+# Split weights in two groups, one with weight decay and the other not.
+no_decay = ["bias", "LayerNorm.weight"]
+optimizer_grouped_parameters = [
+    {
+        "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+        "weight_decay": args.weight_decay,
+    },
+    {
+        "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
+        "weight_decay": 0.0,
+    },
+]
+optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
+
+model, optimizer, train_dataloader = accelerator.prepare(
+    model, optimizer, train_dataloader
 )
