3b08f8faa35af32e61fcb2a15a4256af70a100af
kyle65463 <kyle65463@gmail.com>
2022-05-16 00:05:45 +0800

    Update

--- final/run.sh
+++ final/run.sh
@@ -3,6 +3,6 @@ output_file=$2
 CUDA_VISIBLE_DEVICES=0 python3 inference.py \
     --model_path output/ \
     --num_beams 5 \
-    --batch_size 16 \ 
+    --batch_size 16 \
     --input_file $input_file \
     --output_file $output_file
